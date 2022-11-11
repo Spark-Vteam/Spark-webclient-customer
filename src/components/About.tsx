3b9c849bcb588e568937a902457ef99d832ff225
emcofah <emfh21@student.bth.se>
2022-11-11 14:00:49 +0100

    TypeScript base w. front page shell

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// importing Link from react-router-dom to navigate to 
+import { Link } from "react-router-dom";
+
+
+const About = () => {
+    return (
+        <div className="topnav-right">
+            <Link to="/"> Home</Link>
+            <Link to="/about"> About</Link>
+            <Link to="/policy"> Policy</Link>
+            <p>About</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// importing Link from react-router-dom to navigate to 
+import { Link } from "react-router-dom";
+
+
+const Login = () => {
+    return (
+        <div className="topnav-right">
+            <Link to="/"> Home</Link>
+            <Link to="/about"> About</Link>
+            <Link to="/policy"> Policy</Link>
+            <p>Login</p>
+        </div>
+    );
+};
+
+export default Login;
